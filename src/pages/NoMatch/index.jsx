90f0a913ac1e6f3db1371cb7f8d7fb0488ac366d
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-01-23 01:16:25 -0300

    :sparkles: Página Usuário Não encontrado

--- src/pages/NoMatch/index.jsx
+++ src/pages/NoMatch/index.jsx
@@ -1,3 +1,15 @@
+import { Header } from '../../components/Header';
+
+import styles from './styles.module.scss';
+
 export function NoMatch() {
-  return <div><h1>not found</h1></div>;
+  return (
+    <div className={ styles.NoMatchContainer }>
+      <Header isHome={ false } />
+      <main className={ styles.MainContainer}>
+        <h1>404</h1>
+        <h2>Usuário não encontrado</h2>
+      </main>
+    </div>
+    );
 }
