bdef3e984f3a6d08fec5bfaf962c883e27ee06f4
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-01-22 23:42:45 -0300

    :construction: Estilos globais adicionados

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './App'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { User } from './pages/User'
-import { NoMatch } from './pages/NoMatch'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/global.scss';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-)
+);
